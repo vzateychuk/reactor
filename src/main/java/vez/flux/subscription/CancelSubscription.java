41faa94296d263063056fe2988d1b143fd696656
vzateychuk <vzateychuk@gmail.com>
2022-10-15 15:10:43 +0300

    Cancel subscription after some rounds

---
+++ src/main/java/vez/flux/subscription/CancelSubscription.java
@@ -0,0 +1,26 @@
+package vez.flux.subscription;
+
+import reactor.core.publisher.Flux;
+
+public class CancelSubscription {
+
+  /**
+   * Сначала с помощью оператора range генерируется 100 значений. 2. Выполняется подписка на поток,
+   * так же как это делалось в предыдущем примере. 3. Однако теперь мы управляем подпиской. Сначала
+   * запрашивается четыре элемента (3.1), а потом подписка сразу же отменяется (3.2), то есть другие
+   * элементы вообще не должны генерироваться
+   */
+  public static void main(String[] args) {
+    //
+      Flux.range(1, 100) //(1)
+              .subscribe( // (2)
+                      data -> System.out.println("onNext: " + data ),
+                      err -> { /* игнорируется */ },
+                      () -> System.out.println("onComplete"),
+                      subscription -> { // (3)
+                          subscription.request(4); // (3.1)
+                          subscription.cancel(); // (3.2)
+                      }
+              );
+  }
+}
